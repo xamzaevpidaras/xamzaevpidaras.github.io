ba8202d96bf7344b502f2a2d9599b20501cac906
pidaras xamzaevich <xamzaevpidaras@icloud.com>
2024-10-31 01:50:54 +0300

    кнопка назад

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.getElementById("showButton").addEventListener("click", function() {
-    this.style.display = "none";
-    document.querySelector(".overlay").classList.add("show-overlay");
-  });
-  
+  this.style.display = "none";
+  document.querySelector(".overlay").classList.add("show-overlay");
+});
+
+document.getElementById("backButton").addEventListener("click", function() {
+  document.querySelector(".overlay").classList.remove("show-overlay");
+  document.getElementById("showButton").style.display = "block";
+});
